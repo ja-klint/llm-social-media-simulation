00febd61154d0a6ed36fbbd7915dd6bb4510e0b6
Jakob Klint <19237886+ja-klint@users.noreply.github.com>
2025-12-14 12:39:55 +0100

    Moved to numpy.random and using better method to pick random. Also using seed to replicate picks.

--- generate_agents/agent_generator.py
+++ generate_agents/agent_generator.py
@@ -1,20 +1,28 @@
 import json
 from pathlib import Path
-import random
+import numpy as np
 
 # file with personas to pick from, should be in parent folder
 from_file = 'all_personas.json'
 
 # file to save personas to
-to_file = 'test_agents.jsonl'
+to_file = 'run3_agents.jsonl'
 
-# num_of_democrats = 22
-# num_of_republicans = 22
-# num_of_nonpartisan = 6
-num_of_democrats = 4
-num_of_republicans = 4
-num_of_nonpartisan = 1
+run_id = 3
+
+num_of_democrats = 22
+num_of_republicans = 22
+num_of_nonpartisan = 6
+
+party_counts = {
+    'Democrat': num_of_democrats,
+    'Republican': num_of_republicans,
+    'Non-partisan': num_of_nonpartisan
+}
+
 total_number_of_agents = num_of_democrats + num_of_republicans + num_of_nonpartisan
+
+rng = np.random.default_rng(run_id)
 
 print('Total number of Agents to generate:', total_number_of_agents)
 print(f'Democrats: {100*round(num_of_democrats/total_number_of_agents, 3)}% | Republicans: {100*round(num_of_republicans/total_number_of_agents, 3)}% | Non-partisan: {100*round(num_of_nonpartisan/total_number_of_agents, 3)}%')
@@ -31,6 +39,8 @@ if to_path.is_file():
 try:
     with open(from_path, 'r', encoding='utf-8', errors='ignore') as f:
         agent_data: list = json.load(f)
+    rng.shuffle(agent_data)
+
     with open(to_path, 'w', encoding='utf-8', errors='ignore') as f:
         f.write('')
     
@@ -39,32 +49,26 @@ except:
 
 agent_list = []
 a_id = 1
-while num_of_democrats > 0 or num_of_republicans > 0 or num_of_nonpartisan > 0:
-    random_index = random.randrange(len(agent_data))
-    selected_agent:dict = agent_data.pop(random_index) # avoid duplicates
 
-    agent = {
-        'agent_id': a_id,
-        'party': selected_agent['party'],
-        'bio': selected_agent['bio'],
-        'persona': selected_agent['persona'].rstrip()}
+for selected_agent in agent_data:
 
-    if agent['party'] == 'Democrat' and num_of_democrats > 0:
-        print(f'Selected {agent['party']} #{num_of_democrats}')
-        num_of_democrats -= 1
+    party = selected_agent['party']
 
-    elif agent['party'] == 'Republican' and num_of_republicans > 0:
-        print(f'Selected {agent['party']} #{num_of_republicans}')
-        num_of_republicans -= 1
+    if party_counts[party] > 0:
+        agent = {
+            'agent_id': a_id,
+            'party': selected_agent['party'],
+            'bio': selected_agent['bio'],
+            'persona': selected_agent['persona'].rstrip()}
 
-    elif agent['party'] == 'Non-partisan' and num_of_nonpartisan > 0:
-        print(f'Selected {agent['party']} #{num_of_nonpartisan}')
-        num_of_nonpartisan -= 1
+        print(f'Selected {party} #{party_counts[party]}')
 
-    else:
-        continue # skip if all spots for selected party are taken
+        party_counts[party] -= 1
 
-    with open(to_path, 'a', encoding='utf-8', errors='ignore') as f:
-        f.write(f'{json.dumps(agent, indent=None, ensure_ascii=False)}\n')
+        with open(to_path, 'a', encoding='utf-8', errors='ignore') as f:
+            f.write(f'{json.dumps(agent, indent=None, ensure_ascii=False)}\n')
+        
+        a_id += 1
 
-    a_id += 1
+    if a_id > total_number_of_agents:
+        break
