471f1218f5648d606f93c558e04ffcb2bc309c24
Jakob Klint <19237886+ja-klint@users.noreply.github.com>
2025-12-17 00:02:49 +0100

    Added code to run simulations.

--- simulation/main.py
+++ simulation/main.py
@@ -1,4 +1,20 @@
+from simulation import Simulation
+from dotenv import load_dotenv
+from pathlib import Path
 
-# handle settings for simulation
-# call on simulation to run
-# etc..
+# Load OpenAI api key from path
+load_dotenv(dotenv_path='simulation/api_keys.env')
+
+# Interventions: 'CONTROL', 'SOCIAL_PROOF', 'IDENTITY'
+# intervention = 'CONTROL'
+# intervention = 'SOCIAL_PROOF'
+intervention = 'IDENTITY'
+run_id = 3
+
+num_agents = 50
+num_timesteps = 2000
+
+news_path = Path(__file__).parent.joinpath('News_Category_Dataset_v3.jsonl')
+
+simulation = Simulation(num_agents, num_timesteps, intervention, news_path, run_id=run_id)
+simulation.run()
