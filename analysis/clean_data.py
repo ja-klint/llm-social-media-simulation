c3cb077aff526d3c764ccbadce1b177fb6bfbb5f
ja-klint <19237886+ja-klint@users.noreply.github.com>
2026-01-04 13:49:14 +0100

    Added most metrics to dataframes

--- analysis/clean_data.py
+++ analysis/clean_data.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 from pathlib import Path
 
-def load_data(data_path: Path, agents_path: Path, interventions: list[str], runs: int) -> tuple[pd.DataFrame, pd.DataFrame]:
+def load_data(data_path: Path, agents_path: Path, interventions: list[str], runs: int) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     
     all_timesteps = []
     all_posts = []
@@ -38,28 +38,35 @@ def load_data(data_path: Path, agents_path: Path, interventions: list[str], runs
     
     return (df_timesteps, df_posts, df_agents)
 
-def add_metrics_t(df_t: pd.DataFrame, df_p: pd.DataFrame) -> pd.DataFrame:
+def prepare_t(df_t: pd.DataFrame) -> pd.DataFrame:
     '''
-    Add num_likes (int), num_dislikes (int) and is_follow (0|1) for each timestep.
+    Add is_follow (0|1) and changed some floats to Int64 for each timestep.
     
     :param df_t: Timesteps dataframe
     :type df_t: DataFrame
-    :param df_p: Posts dataframe
-    :type df_p: DataFrame
-    :return: Timesteps dataframe with added metrics
+    :return: Timesteps dataframe with added metrics and some changed datatypes. Columns: ['run_id', 'intervention', 'timestep', 'agent_id', 'action', 'post_id', 'repost_target_id', 'liked_ids', 'disliked_ids', 'followed_agent_id', 'shown_post_ids', 'is_follow']
     :rtype: DataFrame
     '''
 
-    # For timestep dataframe
-    # Count likes and dislikes and add binary is_follow
-    df_t['num_likes'] = df_t['liked_ids'].apply(len)
-    df_t['num_dislikes'] = df_t['disliked_ids'].apply(len)
+    # Change datatype to Int64 for post_id and followed_agent_id, count likes and dislikes and add binary is_follow
+    df_t['post_id'] = df_t['post_id'].astype('Int64')
+    df_t['repost_target_id'] = df_t['repost_target_id'].astype('Int64')
     df_t['is_follow'] = df_t['followed_agent_id'].notna().astype(int)
+    df_t['followed_agent_id'] = df_t['followed_agent_id'].astype('Int64')
 
     return df_t
 
-def add_metrics_p(df_t: pd.DataFrame, df_p: pd.DataFrame) -> pd.DataFrame:
-    # For posts dataframe
+def prepare_p(df_t: pd.DataFrame, df_p: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Add num_likes (int), num_dislikes (int), num_reposts (int), and times_shown (int) for each post.
+    
+    :param df_t: Timesteps dataframe
+    :type df_t: DataFrame
+    :param df_t: Posts dataframe
+    :type df_t: DataFrame
+    :return: Posts dataframe with added metrics. Columns: ['run_id', 'intervention', 'timestep', 'agent_id', 'post_id', 'content', 'num_likes', 'num_dislikes', 'num_reposts', 'times_shown']
+    :rtype: DataFrame
+    '''
 
     # count and add LIKES
     likes = df_t[['run_id', 'intervention', 'liked_ids']]
@@ -94,24 +101,72 @@ def add_metrics_p(df_t: pd.DataFrame, df_p: pd.DataFrame) -> pd.DataFrame:
 
     return df_p
 
-def add_metrics_a(df_t: pd.DataFrame, df_p: pd.DataFrame, df_a: pd.DataFrame) -> pd.DataFrame:
-    # For agents dataframe
-    # columns for agents dataframe: ['run_id', 'agent_id', 'party', 'bio', 'persona', 'following', 'followers', 'written_post_ids', 'liked_post_ids', 'dislikes_post_ids', 'reposted_post_ids']
+def prepare_a(df_t: pd.DataFrame, df_a: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Add following (list), followers (list), written_post_ids (list), reposted_post_ids (list), liked_post_ids (list), and disliked_post_ids (list) for each agent.
     
-    # count and add LIKES
-    follows = df_t[['run_id', 'intervention', 'agent_id', 'followed_agent_id']]
-    follows = follows.dropna()
+    :param df_t: Timesteps dataframe
+    :type df_t: DataFrame
+    :param df_t: Posts dataframe
+    :type df_t: DataFrame
+    :return: Agents dataframe with added metrics. Columns: ['run_id', 'intervention', 'agent_id', 'party', 'bio', 'persona', 'followed', 'followers', 'written_post_ids', 'reposted_post_ids', 'liked_post_ids', 'disliked_post_ids']
+    :rtype: DataFrame
+    '''
     
-    ##
-    ## Add column 'followed', with values list[agent_id, agent_id, ..]
-    ## Add column 'followers', with values list[agent_id, agent_id, ..]
-    ##
-    # likes = follows.rename(columns={'liked_ids': 'post_id'})    
+    # count and add FOLLOWS and FOLLOWERS
+    follows = df_t[df_t['is_follow'] == 1][['run_id', 'intervention', 'agent_id', 'followed_agent_id']]
 
+    # add followed
+    followed = follows.groupby(['run_id', 'intervention', 'agent_id'])['followed_agent_id']
+    followed = followed.aggregate(func=list).reset_index().rename(columns={'followed_agent_id': 'followed'})
 
-    # likes = follows.groupby(['run_id', 'intervention', 'post_id']).size().reset_index(name='num_likes')
-    # df_p = df_p.merge(likes, on=['run_id', 'intervention', 'post_id'], how='left')
-    # print(likes.groupby(['run_id', 'intervention']).describe())
+    df_a = df_a.merge(followed, on=['run_id', 'intervention', 'agent_id'], how='left')
+    df_a['followed'] = df_a['followed'].apply(lambda x: x if isinstance(x, list) else [])
+
+    # add followers
+    followers = follows.groupby(['run_id', 'intervention', 'followed_agent_id'])['agent_id']
+    followers = followers.aggregate(func=list).reset_index().rename(columns={'agent_id': 'followers', 'followed_agent_id': 'agent_id'})
+
+    df_a = df_a.merge(followers, on=['run_id', 'intervention', 'agent_id'], how='left')
+    df_a['followers'] = df_a['followers'].apply(lambda x: x if isinstance(x, list) else [])
+
+    # Add written_post_ids
+    written_posts = df_t[df_t['action'] == 'WRITE_POST'][['run_id', 'intervention', 'agent_id', 'post_id']]
+    
+    written_posts = written_posts.groupby(['run_id', 'intervention', 'agent_id'])['post_id']
+    written_posts = written_posts.aggregate(func=list).reset_index().rename(columns={'post_id': 'written_post_ids'})
+
+    df_a = df_a.merge(written_posts, on=['run_id', 'intervention', 'agent_id'], how='left')
+    
+    df_a['written_post_ids'] = df_a['written_post_ids'].apply(lambda x: x if isinstance(x, list) else [])
+    
+    # Add reposted_post_ids
+    reposted_posts = df_t[df_t['action'] == 'REPOST'][['run_id', 'intervention', 'agent_id', 'repost_target_id']]
+    
+    reposted_posts = reposted_posts.groupby(['run_id', 'intervention', 'agent_id'])['repost_target_id']
+    reposted_posts = reposted_posts.aggregate(func=list).reset_index().rename(columns={'repost_target_id': 'reposted_post_ids'})
+
+    df_a = df_a.merge(reposted_posts, on=['run_id', 'intervention', 'agent_id'], how='left')
+    
+    df_a['reposted_post_ids'] = df_a['reposted_post_ids'].apply(lambda x: x if isinstance(x, list) else [])
+
+    # Add liked_post_ids
+    liked_posts = df_t[['run_id', 'intervention', 'agent_id', 'liked_ids']].explode('liked_ids').dropna()
+    
+    liked_posts = liked_posts.groupby(['run_id', 'intervention', 'agent_id'])['liked_ids']
+    liked_posts = liked_posts.aggregate(func=list).reset_index().rename(columns={'liked_ids': 'liked_post_ids'})
+
+    df_a = df_a.merge(liked_posts, on=['run_id', 'intervention', 'agent_id'], how='left')
+    df_a['liked_post_ids'] = df_a['liked_post_ids'].apply(lambda x: x if isinstance(x, list) else [])
+
+    # Add disliked_post_ids
+    disliked_posts = df_t[['run_id', 'intervention', 'agent_id', 'disliked_ids']].explode('disliked_ids').dropna()
+    
+    disliked_posts = disliked_posts.groupby(['run_id', 'intervention', 'agent_id'])['disliked_ids']
+    disliked_posts = disliked_posts.aggregate(func=list).reset_index().rename(columns={'disliked_ids': 'disliked_post_ids'})
+
+    df_a = df_a.merge(disliked_posts, on=['run_id', 'intervention', 'agent_id'], how='left')
+    df_a['disliked_post_ids'] = df_a['disliked_post_ids'].apply(lambda x: x if isinstance(x, list) else [])
 
     return df_a
 
@@ -127,13 +182,14 @@ if __name__ == '__main__':
 
     df_t, df_p, df_a = load_data(data_path, agents_path, interventions, runs)
 
-    df_t = add_metrics_t(df_t, df_p)
-    df_p = add_metrics_p(df_t, df_p)
-    df_a = add_metrics_a(df_t, df_p, df_a)
+    df_t = prepare_t(df_t)
+    df_p = prepare_p(df_t, df_p)
+    df_a = prepare_a(df_t, df_a)
 
     # print("Columns:", df_t.columns, "\n")
     # print(df_t.head(25))
     # print("Columns:", df_p.columns, "\n")
     # print(df_p.head(25))
-    print("Columns:", df_a.columns, "\n")
-    print(df_a.head(25))
+    # print("Columns:", df_a.columns, "\n")
+    # print(df_a.head(25))
+
